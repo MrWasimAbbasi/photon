ddbac0a46e693be82674c085628cb300c0511f86
MrWasimAbbasi <imwasimabbasi@gmail.com>
2024-03-17 02:14:55 +0500

    dd & db packges added to photon

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -3,11 +3,12 @@
 namespace App\Controllers;
 
 use App\Core\View;
+use DB\DB;
 
 class HomeController
 {
     public function index()
     {
-        View::render('/home/index', ['welcome' => 'Hello World!']);
+        View::render('/home/index', ['welcome' => 'Hello World!', 'users' => $rows]);
     }
 }
